8519a635ba97d0048afef31f3fe91e703b42e611
BBruington <Psychological_Chemist@hotmail.com>
2022-06-07 22:20:13 -0400

    Learning how /when to use useReducer over setState

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,6 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
+
+import createAction from "../utils/reducer/reducer.utils";
 
 import {
   onAuthStateChangedListener,
@@ -11,8 +13,43 @@ export const UserContext = createContext({
   currentUser: null, //init currentUser as null
 });
 
+//this is storing reducer types to be used
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+//you can ...state to return unchanged keys/values
+//since useReducer returns a new object
+const userReducer = (state, action) => {
+
+  //payload stores the value needed to know how to update the object
+  //aka what you are setting in the reducer
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null); //init the value as null
+  //i can immediately pull {currentUser} from state
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = (user) => {
+    dispatch(createAction( USER_ACTION_TYPES.SET_CURRENT_USER, user ));
+    //dispatch can pull which reducers you want to use and call them
+  };
+
   const value = { currentUser, setCurrentUser }; //generate the value going into the context
 
   //bc getAuth (aka user) persists user between refreshes,
@@ -36,3 +73,13 @@ export const UserProvider = ({ children }) => {
   //the provider will wrap around any parts that
   //need the context inside
 };
+
+/* 
+                   -current-
+const useReducer = (state, action) => {
+  return {
+    currentUser:
+  }
+}
+//reducers are functions that return a new object
+*/
