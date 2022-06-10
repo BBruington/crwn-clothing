14eff1a0929e5b8837810bb4f00737d52f1f90c2
BBruington <Psychological_Chemist@hotmail.com>
2022-06-09 21:28:43 -0400

    converting category context into a redux reducer

--- src/store/user/user.types.js
+++ src/store/user/user.types.js
@@ -1,4 +1,4 @@
 //this is storing reducer types to be used
 export const USER_ACTION_TYPES = {
-    SET_CURRENT_USER: "SET_CURRENT_USER",
+    SET_CURRENT_USER: "user/SET_CURRENT_USER",
 };
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -79,13 +79,15 @@ export const getCategoriesAndDocuments = async () => {
     const q = query(collectionRef);
 
     const querySnapshot = await getDocs(q);
-    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-        const { title, items } = docSnapshot.data();
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
-
-    return categoryMap;
+    return querySnapshot.docs.map(docSnapshot => docSnapshot.data());
+    //     .reduce((acc, docSnapshot) => {
+    //     const { title, items } = docSnapshot.data();
+    //     acc[title.toLowerCase()] = items;
+    //     return acc;
+    // }, {}); 
+    
+    // return categoryMap;
+    //THIS WAS MOVED TO category.selector.js
 }
 
 export const createUserDocumentFromAuth = async (
