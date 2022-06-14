9721e16bcaac5a661905327a6c52ec2d79e63d16
BBruington <Psychological_Chemist@hotmail.com>
2022-06-14 15:59:53 -0400

    setting up serverless functions

--- src/index.js
+++ src/index.js
@@ -3,14 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter  } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import reportWebVitals from './reportWebVitals';
-// import {UserProvider} from './contexts/user.context'
-// import {CategoriesProvider} from './contexts/categories.context'
-// import {CartProvider} from './contexts/cart.context'
 
 import './index.scss';
 
@@ -20,13 +19,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          {/* <UserProvider> */}
-            {/* <CategoriesProvider> */}
-              {/* <CartProvider> */}
-                <App />
-              {/* </CartProvider> */}
-            {/* </CategoriesProvider> */}
-          {/* </UserProvider> */}
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -18,10 +18,7 @@ export const userReducer = (state = INITIAL_STATE, action ) => {
   
     switch (type) {
       case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-        return {
-          ...state,
-          currentUser: payload,
-        };
+        return {...state, currentUser: payload, };
         case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
           return { ...state, currentUser: null };
         case USER_ACTION_TYPES.SIGN_OUT_FAILED:
